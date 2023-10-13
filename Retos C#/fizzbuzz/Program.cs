1553c28ae65349861abf7089861062364e67a693
Angel J. Brito <angeldejesus.bg2005@gmail.com>
2023-10-13 00:43:10 -0400

    FizzBuzz Completado

---
+++ Retos C#/fizzbuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class FIzzBuzz
+    {
+        static void Main(string[] args)
+        {
+            for (int o = 1; o <= 100; o++)
+            {
+                if (o % 3 == 0 && o % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz\n");
+                }
+                else if (o % 3 == 0)
+                {
+                    Console.WriteLine("fizz\n");
+                }
+                else if (o % 5 == 0)
+                {
+                    Console.WriteLine("buzz\n");
+                }
+
+                else
+                {
+                    Console.WriteLine(o + "\n");
+                }
+
+
+            }
+
+        }
+    }
+}
