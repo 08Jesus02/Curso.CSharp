b6da8ec3fffdcf23f1eabb74fb4122a7e15002fe
angelj.brito <20230071@itla.edu.do>
2023-11-26 01:22:31 -0400

    video 8

--- Curso/Sintaxis Basica/Program.cs
+++ Curso/Sintaxis Basica/Program.cs
@@ -6,8 +6,16 @@ namespace SintaxisBasica
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Introduce el primer numero");
-            Console.ReadLine();
+            const double PI = 3.1416;
+
+            Console.WriteLine("Introduce el radio del circulo:");
+            double diametro = double.Parse(Console.ReadLine());
+
+            double radio = diametro / 2;
+            //double Area = PI * radio * radio;
+            double Area = Math.Pow(radio, 2)*PI;
+            Console.WriteLine($"El Area es del circulo es: {Area}");
+            
         }
     }
 }
