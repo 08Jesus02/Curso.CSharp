3f9c40b766dfcafe6d52a87b4563581d2fdc3dde
Angel J. Brito <angeldejesus.bg2005@gmail.com>
2023-11-25 21:58:58 -0400

    primer commit

---
+++ Curso/Sintaxis Basica/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SintaxisBasica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var edadPersona = "Hola";
+
+            
+        }
+    }
+}
